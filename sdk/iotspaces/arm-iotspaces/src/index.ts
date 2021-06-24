5e3b6d37d570ba71a421c28de58b60a639f3c34e
Wei Dong <dw511214992@gmail.com>
2021-06-24 20:15:36 +0800

    latest code

--- sdk/iotspaces/arm-iotspaces/src/index.ts
+++ sdk/iotspaces/arm-iotspaces/src/index.ts
@@ -1,5 +1,3 @@
-/// <reference lib="esnext.asynciterable" />
 export * from "./models";
 export { IoTSpacesClient } from "./ioTSpacesClient";
 export { IoTSpacesClientContext } from "./ioTSpacesClientContext";
-export * from "./operationsInterfaces";
--- sdk/iotspaces/arm-iotspaces/src/ioTSpacesClientContext.ts
+++ sdk/iotspaces/arm-iotspaces/src/ioTSpacesClientContext.ts
@@ -3,26 +3,17 @@ import * as coreAuth from "@azure/core-auth";
 import { IoTSpacesClientOptionalParams } from "./models";
 
 export class IoTSpacesClientContext extends coreClient.ServiceClient {
-  $host: string;
-  apiVersion: string;
-  subscriptionId: string;
-
   /**
    * Initializes a new instance of the IoTSpacesClientContext class.
    * @param credentials Subscription credentials which uniquely identify client subscription.
-   * @param subscriptionId The subscription identifier.
    * @param options The parameter options
    */
   constructor(
     credentials: coreAuth.TokenCredential,
-    subscriptionId: string,
     options?: IoTSpacesClientOptionalParams
   ) {
     if (credentials === undefined) {
       throw new Error("'credentials' cannot be null");
-    }
-    if (subscriptionId === undefined) {
-      throw new Error("'subscriptionId' cannot be null");
     }
 
     // Initializing default values for options
@@ -39,9 +30,6 @@ export class IoTSpacesClientContext extends coreClient.ServiceClient {
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
         : `${packageDetails}`;
-    options.userAgentOptions = {
-      userAgentPrefix: userAgentPrefix
-    };
 
     if (!options.credentialScopes) {
       options.credentialScopes = ["https://management.azure.com/.default"];
@@ -49,14 +37,11 @@ export class IoTSpacesClientContext extends coreClient.ServiceClient {
     const optionsWithDefaults = {
       ...defaults,
       ...options,
-      baseUri: options.endpoint || "https://management.azure.com"
+      userAgentOptions: {
+        userAgentPrefix
+      },
+      baseUri: options.endpoint
     };
     super(optionsWithDefaults);
-    // Parameter assignments
-    this.subscriptionId = subscriptionId;
-
-    // Assigning values to Constant parameters
-    this.$host = options.$host || "https://management.azure.com";
-    this.apiVersion = options.apiVersion || "2017-10-01-preview";
   }
 }
